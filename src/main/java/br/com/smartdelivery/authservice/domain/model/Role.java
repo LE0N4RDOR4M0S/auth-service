546c33483fa60dab3e5b6839ebe1e9465bf03f7e
Leonardo Ramos <leomatisa1000@gmai.com>
2025-08-01 12:51:08 -0400

    feat(auth-domain): criação das entidades de domínio User e Role com estrutura hexagonal

---
+++ src/main/java/br/com/smartdelivery/authservice/domain/model/Role.java
@@ -0,0 +1,45 @@
+package br.com.smartdelivery.authservice.domain.model;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class Role {
+    private UUID id;
+    private String name;
+
+    public Role() {
+    }
+
+    public Role(UUID id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return Objects.equals(id, role.id) && Objects.equals(name, role.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
---
+++ src/main/java/br/com/smartdelivery/authservice/domain/model/User.java
@@ -0,0 +1,94 @@
+package br.com.smartdelivery.authservice.domain.model;
+
+import java.util.*;
+
+public class User {
+    private UUID id;
+    private String name;
+    private String email;
+    private String password;
+    private Date createdAt;
+    private Date updatedAt;
+    private Set<Role> roles;
+
+    public User() {
+    }
+
+    public User(UUID id, String name, String email, String password, Date createdAt, Date updatedAt, Set<Role> roles) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.roles = roles;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(email, user.email) && Objects.equals(password, user.password) && Objects.equals(createdAt, user.createdAt) && Objects.equals(updatedAt, user.updatedAt) && Objects.equals(roles, user.roles);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, password, createdAt, updatedAt, roles);
+    }
+}
