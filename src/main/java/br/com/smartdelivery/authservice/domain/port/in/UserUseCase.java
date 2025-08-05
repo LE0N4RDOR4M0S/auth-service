920908481482bed03d81ca4705d786b81ba0d1d0
Leonardo Ramos <leomatisa1000@gmai.com>
2025-08-05 08:47:17 -0400

    fix: Criaçao de porta primária para User

---
+++ src/main/java/br/com/smartdelivery/authservice/domain/port/in/UserUseCase.java
@@ -0,0 +1,11 @@
+package br.com.smartdelivery.authservice.domain.port.in;
+
+import br.com.smartdelivery.authservice.domain.model.User;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserUseCase {
+    User createUser(User user);
+    Optional<User> getUserById(UUID id);
+}
