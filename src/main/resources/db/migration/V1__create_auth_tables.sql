e2a2f9814afe396485883e9173e46480823de3f3
Leonardo Ramos <leomatisa1000@gmai.com>
2025-08-05 11:59:05 -0400

    fix: uso de palavra reservada do postgres

--- src/main/resources/db/migration/V1__create_auth_tables.sql
+++ src/main/resources/db/migration/V1__create_auth_tables.sql
@@ -5,7 +5,7 @@ CREATE TABLE role (
     name VARCHAR(50) NOT NULL UNIQUE
 );
 
-CREATE TABLE user (
+CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
