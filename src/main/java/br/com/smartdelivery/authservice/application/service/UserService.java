652fce11a24814963c868a8ae6f48bb4714c817a
Leonardo Ramos <leomatisa1000@gmai.com>
2025-08-05 09:23:28 -0400

    feat: Criação do Service de User

---
+++ src/main/java/br/com/smartdelivery/authservice/application/service/UserService.java
@@ -0,0 +1,27 @@
+package br.com.smartdelivery.authservice.application.service;
+
+import br.com.smartdelivery.authservice.domain.model.User;
+import br.com.smartdelivery.authservice.domain.port.in.UserUseCase;
+import br.com.smartdelivery.authservice.domain.port.out.UserRepository;
+import br.com.smartdelivery.authservice.infrastructure.adapter.out.persistence.repository.JpaUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserUseCase {
+    private final JpaUserRepository userRepository;
+
+    @Override
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public Optional<User> getUserById(UUID id) {
+        return userRepository.findById(id);
+    }
+}
